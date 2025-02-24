d75bc8957ce13643a8dafe6cc260895340f67865
Gilary Franco <andy_gfg@hotmail.com>
2025-02-24 15:52:45 -0600

    responsiveness on welcome

--- app/screen/landing/Welcome.tsx
+++ app/screen/landing/Welcome.tsx
@@ -8,39 +8,86 @@ import dot2 from './assets/carrot.svg';
 import dot3 from './assets/bread.svg';
 import dot4 from './assets/cherry.svg';
 
+const HabitsCard = () => (
+  <div className="bg-white rounded-[30px] p-3 flex flex-col items-center min-w-[200px]">
+    <p className="mb-6 w-[90%]">
+      <b className="text-[50px] 2xl:text-[61px]">95%</b>
+
+      <br />
+
+      <span className="text-[20px] 2xl:text-[26px]">
+        Improved Eating Habits
+      </span>
+    </p>
+
+    <img alt="" src={habits} />
+  </div>
+);
+
+const GroceriesCard = () => (
+  <div className="flex xl:items-end order-2">
+    <p className={'bg-white rounded-[30px] min-w-[200px] p-3 flex flex-col '
+      + ' lg:order-none lg:leading-normal'
+    }>
+      <img alt="" className="mb-6" src={groceries} />
+
+      <b className="text-[50px] 2xl:text-[61px]">25%</b>
+
+      <span className="text-[20px] 2xl:text-[26px]">Saved on Groceries</span>
+    </p>
+  </div>
+);
+
+const Food1Card = () => (
+  <div className="hidden self-end min-w-[200px] xl:block">
+    <img alt="" src={food1} />
+  </div>
+);
+
+const Food2Card = () => (
+  <div className="hidden min-w-[200px] xl:block">
+    <img alt="" src={food2} />
+  </div>
+);
+
 const Welcome = () => {
   return (
-    <div className="flex flex-col items-center bg-custom-light relative pb-20">
+    <div className="flex flex-col items-center bg-custom-light relative pb-20 px-10">
       <h1 className="max-w-[800px] text-center">
         Elevate Your Mealtime with AI-Powered Personalization
       </h1>
 
-      <div className="flex flex-row gap-8">
-        <div>
-          <div className="bg-white rounded-[30px] p-3 flex flex-col items-center">
-            <p className="mb-6 w-[90%]">
-              <b className="text-[61px]">95%</b>
-
-              <br />
-
-              <span className="text-[26px]">
-                Improved Eating Habits
-              </span>
-            </p>
-
-            <img alt="" src={habits} />
-          </div>
+      <div
+        className={'flex flex-col gap-8 items-center'
+          + ' lg:flex-row lg:items-start '
+        }
+      >
+        <div className="max-xl:hidden flex">
+          <HabitsCard />
         </div>
 
         <div className="flex flex-col">
           <p className="mt-5 mb-15 text-center text-[26px]">Effortless Planning, Healthier Eating</p>
 
-          <div className="flex flex-row gap-8">
-            <div>
-              <img alt="" src={food1} />
+          <div
+            className={'flex flex-row gap-8 flex-wrap justify-center'
+              + ' md:flex-nowrap'}
+          >
+            <Food1Card />
+
+            <div
+              className={'flex xl:hidden order-3 md:order-none'
+                + ' '}
+            >
+              <HabitsCard />
             </div>
 
-            <div className="flex flex-col items-center">
+            <div
+              className={'flex flex-col items-center order-1 '
+                + 'lg:order-none'
+                + ' max-md:w-[100%]'
+              }
+            >
               <div className="flex flex-col gap-10">
                 <Button label="Try Our Demo" variant="light" />
 
@@ -48,35 +95,28 @@ const Welcome = () => {
               </div>
 
               <p className={'flex flex-col bg-custom-green rounded-[34px] text-custom-light-green'
-                + ' w-[390px] text-center text-[61px] py-10 mt-20'}>
+                + ' w-[300px] text-center text-[50px] py-10 mt-20'
+                + ' 2xl:text-[61px] lg:w-[390px]'}>
                 <b>30 000 +</b>
 
                 <span className="text-[30px]">Happy users</span>
               </p>
             </div>
 
-            <div className="bg-white rounded-[30px] p-3 flex flex-col items-center">
-              <p>
-                <img alt="" className="mb-6" src={groceries} />
-
-                <b className="text-[61px]">25%</b>
-
-                <br />
-
-                <span className="text-[26px]">Saved on Groceries</span>
-              </p>
-            </div>
+            <GroceriesCard />
           </div>
         </div>
 
         <div>
-          <div>
-            <img alt="" src={food2} />
-          </div>
+          <Food2Card />
         </div>
       </div>
 
-      <div className="absolute flex justify-between w-[1100px] mt-[70px]">
+      <div
+        className={'absolute hidden justify-between max-w-[1100px] mt-[70px]'
+          + ' xl:w-[90%]'
+          + ' lg:flex lg:w-[86%]'}
+      >
         <div className="h-[200px] flex items-center">
           <img alt="" src={dot1} className="self-start" />
 
